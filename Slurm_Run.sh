d2f55ca2a531c11f30fc27132ee10f4ef25131c3
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2025-07-14 10:31:57 -0400

    revert because it feels like a temporary slurm rather than a current workflow problem

--- Slurm_Run.sh
+++ Slurm_Run.sh
@@ -3,7 +3,8 @@
 echo "Submitting slurm job. Remember to edit the Unifier_Run.sh if you want to make changes."
 
 sbatch \
-  -G 1 --mem=600G \
+  -n 8 \
+  -c 10 -G 1 --mem=600G \
   --time=28800 \
   -o dataprep.log \
   Unified-bee-Runner/Unifier_Run.sh
