5894e5d94e2916ff2bdff56419bd6477895267b4
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2025-07-14 10:02:31 -0400

    fix some requirements stopping slurm from running

--- Slurm_Run.sh
+++ Slurm_Run.sh
@@ -3,8 +3,7 @@
 echo "Submitting slurm job. Remember to edit the Unifier_Run.sh if you want to make changes."
 
 sbatch \
-  -n 8 \
-  -c 10 -G 1 --mem=600G \
+  -G 1 --mem=600G \
   --time=28800 \
   -o dataprep.log \
   Unified-bee-Runner/Unifier_Run.sh
