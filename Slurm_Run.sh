8d8a9272ff5f2bdab9aa7a07270b5c96c1004228
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2025-07-25 10:20:07 -0400

    Sustainable requirements (#139)
    * reduce requirements to mollify the slurm gods
    
    * update reminder info

--- Slurm_Run.sh
+++ Slurm_Run.sh
@@ -3,10 +3,10 @@
 echo "Submitting slurm job. Remember to edit the Unifier_Run.sh (especially if you want to change input/output) if you want to make changes."
 
 sbatch \
-  -n 8 \
-  -c 10 -G 1 --mem=600G \
+  -n 4 \
+  -c 5 -G 1 --mem=400G \
   --time=28800 \
   -o "/dev/null" \
   Unified-bee-Runner/Unifier_Run.sh
 
-echo "Slurm Submitted"
+echo "Slurm job submitted. Run the 'squeue' command to see if it got submitted!"
